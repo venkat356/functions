ebe46f202a2762f285e00845835978b4146252ef
venkat356 <113495824+venkat356@users.noreply.github.com>
2023-05-19 18:42:22 +0530

    added today's work

---
+++ src/CheckEvenNumber.java
@@ -0,0 +1,18 @@
+import java.util.function.Function;
+     public class CheckEvenNumber{
+
+        public static void main(String[] args) {
+
+            Function<Integer, Boolean> mul = x -> x % 2 == 0;
+            System.out.println(f1(mul));
+
+        }
+        static boolean f1(Function<Integer, Boolean> fun) {
+            boolean b = fun.apply(35);
+            return b;
+
+        }
+
+
+    }
+
